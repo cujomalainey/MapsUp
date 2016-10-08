30a860f4b7a4eaf73a31679e0710a8d8c8799bd4
Curtis Malainey <curtis@malainey.com>
2016-10-08 00:39:44 -0400

    Initial matrix config

---
+++ teensy/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#include <Fonts/FreeMono9pt7b.h>
+
+#define PIN 6
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(20);
+  matrix.setTextColor(matrix.Color(255, 0, 0));
+}
+
+void loop() {
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 0);
+  matrix.print(F("Jesse"));
+  matrix.show();
+  delay(100);
+}
