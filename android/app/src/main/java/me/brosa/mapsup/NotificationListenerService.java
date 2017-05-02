af600ebf86df47e2c30254544416f8c3b8d210df
Jesse Farebrother <jessefarebro@gmail.com>
2017-05-01 19:34:01 -0600

    Fixes messages longer than 20 bytes with simple message splitting

--- android/app/src/main/java/me/brosa/mapsup/NotificationListenerService.java
+++ android/app/src/main/java/me/brosa/mapsup/NotificationListenerService.java
@@ -35,6 +35,8 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
+import static android.bluetooth.BluetoothGatt.GATT_SUCCESS;
+
 
 public class NotificationListenerService extends android.service.notification.NotificationListenerService {
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
@@ -66,6 +68,7 @@ public class NotificationListenerService extends android.service.notification.No
 
     private volatile boolean mScanActive = false;
     private boolean mConnected = false;
+    private boolean inFlight = false;
 
     private final BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
         @Override
@@ -95,13 +98,13 @@ public class NotificationListenerService extends android.service.notification.No
                 Log.d(DEBUG_TAG, "Disconnected from Bluetooth");
                 mConnected = false;
                 mBtDevice = null;
-                scanForDevices();
+                //scanForDevices();
             }
         }
 
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-            if (status == BluetoothGatt.GATT_SUCCESS) {
+            if (status == GATT_SUCCESS) {
                 Log.d(DEBUG_TAG, "GATT Status success");
                 BluetoothGattService service = mBtGatt.getService(NotificationListenerService.UUID_BLE_SHIELD_SERVICE);
                 if (service != null) {
@@ -111,6 +114,17 @@ public class NotificationListenerService extends android.service.notification.No
             } else {
                 Log.w(DEBUG_TAG, "onServicesDiscovered received: " + status);
             }
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            inFlight = false;
+            if (status != GATT_SUCCESS) {
+                Log.e(DEBUG_TAG, "Failed to write characteristic ");
+            } else {
+                Log.d(DEBUG_TAG, "SUCCESS");
+            }
+            super.onCharacteristicWrite(gatt, characteristic, status);
         }
     };
 
@@ -217,21 +231,21 @@ public class NotificationListenerService extends android.service.notification.No
         RelativeLayout rl = (RelativeLayout) rv.apply(getApplicationContext(), null);
         ImageView turnArrow = (ImageView) rl.getChildAt(0);
 
-        Log.d(DEBUG_TAG, Integer.toHexString(turnArrow.hashCode()));
-        Log.d(DEBUG_TAG, Integer.toHexString(turnArrow.getDrawable().getConstantState().hashCode()));
+        //Log.d(DEBUG_TAG, Integer.toHexString(turnArrow.hashCode()));
+        //Log.d(DEBUG_TAG, Integer.toHexString(turnArrow.getDrawable().getConstantState().hashCode()));
 
         LinearLayout ll = (LinearLayout) ((LinearLayout) rl.getChildAt(1)).getChildAt(0);
         TextView distance = (TextView) ll.getChildAt(0);
         TextView direction = (TextView) ll.getChildAt(1);
         TextView eta = (TextView) ll.getChildAt(2);
 
-        Log.d(DEBUG_TAG, distance.getText().toString());
-        Log.d(DEBUG_TAG, direction.getText().toString());
-        Log.d(DEBUG_TAG, eta.getText().toString());
+        //Log.d(DEBUG_TAG, distance.getText().toString());
+        //Log.d(DEBUG_TAG, direction.getText().toString());
+        //Log.d(DEBUG_TAG, eta.getText().toString());
 
         if (mConnected && mBtGattService != null) {
-            BluetoothGattCharacteristic cr = mBtGattService.getCharacteristic(NotificationListenerService.UUID_BLE_SHIELD_TX);
 
+            BluetoothGattCharacteristic cr = mBtGattService.getCharacteristic(NotificationListenerService.UUID_BLE_SHIELD_TX);
             GoogleMapsDirection.TurnDirection d = null;
             String directionString = direction.getText().toString();
 
@@ -254,15 +268,45 @@ public class NotificationListenerService extends android.service.notification.No
 
             GoogleMapsDirection payload = new GoogleMapsDirection.Builder()
                     .distance(distance.getText().toString().replaceAll("\\s+",""))
+                    .distance("Hello world this is a test of distance!!!!!!")
                     .tdirection(d)
                     .build();
-            if (payload.hashCode() == mLastTransmitionCode)
-                return;
+            /*if (payload.hashCode() == mLastTransmitionCode)
+                return;*/
             mLastTransmitionCode = payload.hashCode();
+            byte payloadEncoding[] = payload.encode();
+            byte payloadFrame[] = new byte[payloadEncoding.length + 4];
+            payloadFrame[0] = new Integer(0xA5).byteValue();
+            payloadFrame[1] = new Integer(payloadEncoding.length).byteValue();
+            payloadFrame[2] = 0;
 
-            Log.d(DEBUG_TAG, "Sending " + String.valueOf(payload.encode().length) + " bytes of data");
-            cr.setValue(payload.encode());
-            mBtGatt.writeCharacteristic(cr);
+            int checksum = 0xA5 ^ payloadEncoding.length;
+            for (int i = 0; i < payloadEncoding.length; i++) {
+                payloadFrame[3 + i] = payloadEncoding[i];
+                checksum ^= payloadEncoding[i];
+            }
+            payloadFrame[payloadFrame.length - 1] = new Integer(checksum).byteValue();
+
+            int remainingBytes = payloadFrame.length;
+            int bufferPtr = 0;
+            for (int i = 0; i < (int)(payloadFrame.length / 20) + 1; i++) {
+                byte currentFrame[] = new byte[Math.min(remainingBytes, 20)];
+                for (int j = 0; j < currentFrame.length; j++) {
+                    currentFrame[j] = payloadFrame[bufferPtr++];
+                    remainingBytes--;
+                }
+                cr.setValue(currentFrame);
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (!mBtGatt.writeCharacteristic(cr)) {
+                    inFlight = true;
+                    Log.e(DEBUG_TAG, "Failed to initiate write of characteristic");
+                }
+                Log.d(DEBUG_TAG, "Wrote frame of " + String.valueOf(currentFrame.length) + " bytes");
+            }
         }
     }
 }
